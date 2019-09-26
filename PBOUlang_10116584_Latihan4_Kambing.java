df27d58f2b2959e3ea88a5d3f6fea6796e2273e0
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:47:44 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan4_Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan4_Kambing;
+
+/**
+ *
+ * @author 
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan4_Kambing {
+    public void tambahKambing(){
+        // Deklarasi Variabel Lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah ditambah : " + jumlahKambing);
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOUlang_10116584_Latihan4_Kambing kambingJantan = new PBOUlang_10116584_Latihan4_Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
